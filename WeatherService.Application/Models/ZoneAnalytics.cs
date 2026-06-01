d20d35a02292ac7f19de72447b5bff642b0ff454
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2026-06-01 12:29:58 -0600

    Create ZoneAnalytics application domain models

---
+++ WeatherService.Application/Models/ZoneAnalytics.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace WeatherService.Application.Models
+{
+    public class ZonePoint
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class HistoricalRecord
+    {
+        public DateTime Timestamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Temperature { get; set; }
+        public double WindSpeed { get; set; }
+        public int WindDirection { get; set; }
+    }
+
+    public class ZoneAnalytics
+    {
+        public string ZoneName { get; set; } = string.Empty;
+        public List<ZonePoint> Points { get; set; } = new();
+        public List<HistoricalRecord> History { get; set; } = new();
+    }
+}
